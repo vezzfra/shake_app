b7bd495d3d1aa69b5935fc69b407a7399384f626
Francesco <Francesco@iMac-di-Francesco-2.local>
2015-05-25 20:33:51 +0200

    Initial Commit

---
+++ Shake_AppTests/Shake_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shake_AppTests.swift
+//  Shake_AppTests
+//
+//  Created by Francesco on 25/05/15.
+//  Copyright (c) 2015 Vezzoli Development. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Shake_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
